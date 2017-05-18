6467eb1408c95541ae4e88dcab4090165ab7d210
Elaine Paiva <Elaine Paiva>
2017-05-18 13:47:09 -0300

    cursor:pointer (n pegou no rodape)

---
+++ mustachio.js
@@ -0,0 +1,7 @@
+var mudaImagem = document.querySelector("#pinterest");
+
+mudaImagem.addEventListener ('mouseover', function() {
+
+    mudaImagem = ('https://image.freepik.com/icones-gratis/forma-do-logotipo-do-twitter_318-55611.jpg'); 
+    
+})
