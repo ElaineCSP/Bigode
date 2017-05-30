bc51cc9ea40cf412f0f82c8e130021c074a9ae24
Elaine Paiva <Elaine Paiva>
2017-05-29 23:48:08 -0300

    botao enviando dados

--- mustachio.js
+++ mustachio.js
@@ -1,12 +1,31 @@
 
+var nome;
+var endereço;
+var telefone;
+var email;
+var mensagem;
 
- function validacao() {
+function enviar() { 
+     
+    nome = document.form.nome.value;
+    endereco = document.form.endereco.value;
+    telefone = document.form.telefone.value;
+    email = document.form.email.value;
+    mensagem = document.form.mensagem.value;
 
-    console.log(document.form.nome.value);
-     console.log(document.form.endereco.value);
-     console.log(document.form.telefone.value);
-     console.log(document.form.email.value);
-     console.log(document.form.mensagem.value);
+    console.log(nome);
+    console.log(endereco);
+    console.log(telefone);
+    console.log(email);
+    console.log(mensagem)
+
+    // document.formfeio.nome.value = nome;
+    // document.formfeio.endereco.value = endereco;
+    // document.formfeio.email.value = telefone;
+    // document.formfeio.telefone.value = email;
+    // document.formfeio.mensagem.value = mensagem;
     
     return false;
 } 
+
+
